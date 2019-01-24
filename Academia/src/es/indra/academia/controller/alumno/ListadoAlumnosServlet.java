92a55b5f78401b84a8361aa78d0348681cc203e4
CURSOJAVA <CURSOJAVA@192.168.1.33>
2019-01-24 20:32:17 +0100

    Actualización

--- Academia/src/es/indra/academia/controller/alumno/ListadoAlumnosServlet.java
+++ Academia/src/es/indra/academia/controller/alumno/ListadoAlumnosServlet.java
@@ -1,39 +1,57 @@
 package es.indra.academia.controller.alumno;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import es.indra.academia.model.entities.Alumno;
+import es.indra.academia.model.service.AlumnoService;
+
 /**
- * Servlet implementation class ListadoAlumnosServlet
+ * Servlet implementation class ListarAlumnosServer
  */
-@WebServlet("/admin/alumnos/listado")
+@WebServlet("/admin/alumnos/listado.html")
 public class ListadoAlumnosServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ListadoAlumnosServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public ListadoAlumnosServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		AlumnoService alumnoService = AlumnoService.getInstance();
+
+		List<Alumno> alumnos = alumnoService.findAll();
+		request.setAttribute("listado", alumnos);
+
+		RequestDispatcher dispacher = request.getRequestDispatcher("/WEB-INF/jsp/alumnos/listado.jsp");
+		dispacher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
