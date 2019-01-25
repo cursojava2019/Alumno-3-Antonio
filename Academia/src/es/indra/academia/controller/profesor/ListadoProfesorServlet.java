ad07db6433810d81be6e36d855cc752e06c57dcc
CURSOJAVA <CURSOJAVA@192.168.1.33>
2019-01-25 20:28:30 +0100

    Actualizacion

---
+++ Academia/src/es/indra/academia/controller/profesor/ListadoProfesorServlet.java
@@ -0,0 +1,46 @@
+package es.indra.academia.controller.profesor;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ListadoProfesorServlet
+ */
+@WebServlet("/admin/profesor/listado.html")
+public class ListadoProfesorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ListadoProfesorServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Academia/src/es/indra/academia/model/dao/ProfesorDao.java
@@ -0,0 +1,8 @@
+package es.indra.academia.model.dao;
+
+import es.indra.academia.model.entities.Profesor;
+import es.indra.academia.model.support.Dao;
+
+public class ProfesorDao implements Dao<Long, Profesor> {
+
+}
